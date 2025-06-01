f02a5d6d4b303bbf667694dd3aac6128a1b1ac72
mintusyaka <stanislav.mintus.pz.2022@lpnu.ua>
2025-06-01 21:39:15 +0300

    FIX: routes fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/lyceum-frontend" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
